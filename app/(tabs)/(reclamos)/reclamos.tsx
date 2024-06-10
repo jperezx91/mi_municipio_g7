53347c3f0e6e0a115ac5a07a25c0a21748fea03f
juan <jmperez091@gmail.com>
2024-06-09 21:27:58 -0300

    fix reclamos item(temp)

--- app/(tabs)/(reclamos)/reclamos.tsx
+++ app/(tabs)/(reclamos)/reclamos.tsx
@@ -47,6 +47,15 @@ export default function HomeScreen() {
         }
 
     ]
+    interface itemType
+    {
+        id: number
+        numero: string
+        categoria: string
+        estado: string
+    }
+
+    // @ts-ignore
     const renderItemPublicaion = ({item}) => (
         <ReclamoComponente numero={item.numero} categoria={item.categoria} estado={item.estado} goToPublicacion={() => {
             const idItem : string = item.id
@@ -57,7 +66,7 @@ export default function HomeScreen() {
     return (
         <SafeAreaView style={[PrincipalStyle.principalContainer, {backgroundColor: "#F2F4F8"}]}>
         <View style={{marginTop: 15, padding: 20, paddingTop: 0, backgroundColor: '#F2F4F8', height: Dimensions.get('window').height * 0.80}}>
-            
+
             {/* Botón Mis reclamos */}
             <Pressable style={{paddingTop: 14, paddingBottom:14}} onPress={()=> {router.push("misPublicaciones")}}>
                 <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
@@ -65,7 +74,7 @@ export default function HomeScreen() {
                     <Entypo name="chevron-thin-right" size={20} color="black" />
                 </View>
             </Pressable>
-            
+
             <View style={{
                                 display:'flex',
                                 flexDirection:'row',
@@ -82,18 +91,18 @@ export default function HomeScreen() {
                 backgroundColor:'white',
                 padding:10,
                 marginVertical:10,
-                marginTop:15, 
+                marginTop:15,
                 borderRadius:10,
                 borderWidth:1,
                 borderColor:'#747375',
                 width:'88%'
             }}>
                 <AntDesign name="search1" size={20} color="black" />
-                <TextInput 
-                    placeholder='Busque un reclamo...' 
-                    style={{fontFamily:'outfit'}} 
+                <TextInput
+                    placeholder='Busque un reclamo...'
+                    style={{fontFamily:'outfit'}}
                     clearButtonMode='always'
-                >                    
+                >
                 </TextInput>
             </View>
             <AntDesign name="filter" size={25} color="black"/>
